5665b8c6eb109b344108d1733f5d41bdc4d191fd
ansariii333 <ansarnis99@gmail.com>
2026-03-08 23:50:54 +0500

    Add basic Telegram bot functionality

---
+++ code1.py
@@ -0,0 +1,19 @@
+import telebot
+    
+bot = telebot.TeleBot("8088071512:AAEqopdAWGoRwnANpUM5fVVZqGKH_WWhP1M")
+    
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+    
+    # Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+        count_heh = int(message.text.split()[1]) if len(message.text.split())>1 else 5
+        bot.reply_to(message,"he"*count_heh)
+@bot.message_handler(commands=["ping"])
+def on_ping(message):
+    bot.reply_to(message, "Still alive and kicking!")
+
+    # Запуск бота
+bot.polling()
